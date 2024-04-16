6edfac86b5b81eb64b808d6b6631673245dcf74d
Ximena Islas <karla.islas213090@potros.itson.edu.mx>
2024-04-16 16:17:45 -0700

    componente tabla con datos ficticios

--- src/app/pages/dominios/dominios.component.ts
+++ src/app/pages/dominios/dominios.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { TableModel } from '../../models/table';
+import { TableComponent } from '../../modules/table/table.component';
 
 @Component({
   selector: 'app-dominios',
   standalone: true,
-  imports: [],
+  imports: [TableComponent],
   templateUrl: './dominios.component.html',
   styleUrl: './dominios.component.scss'
 })
 export class DominiosComponent {
+  tableColumns: TableModel[] = [
+    { header: 'ID', field: 'id' },
+    { header: 'Dominio', field: 'dominio' }
+  ];
 
+  tableData = [
+    { id: 1, dominio: 'Contexto Demográfico' },
+    { id: 2, dominio: 'Supervivencia' },
+    { id: 3, dominio: 'Desarrollo' },
+    { id: 4, dominio: 'Protección' },
+  ];
 }
